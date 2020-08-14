c3bb87c024a36fe27a21231f8d87def71c84ec42
chea-young <study.lchy@gmail.com>
2020-08-14 14:40:44 +0900

    MODIFY 네트워크.py

--- programmers/python/level_3/네트워크.py
+++ programmers/python/level_3/네트워크.py
@@ -2,15 +2,18 @@ def solution(n, computers):
     connect = []
     for i in range(len(computers)):
         for j in range(len(computers[i])):
-            if(i == j):
-                continue
-            elif(i<j):
-                if([i,j] not in connect): 
-                    connect.append([i,j])
-            else:
-                if([j,i] not in connect):
-                    connect.append([j,i])
+            if(computers[i][j]==1):
+                if(i == j):
+                    continue
+                elif(i<j):
+                    if([i,j] not in connect): 
+                        connect.append([i,j])
+                else:
+                    if([j,i] not in connect):
+                        connect.append([j,i])
     connect.sort()
+    check = [-1]*n
+    count = 0
     print("connect",connect)
     answer = []
     for i in range(n):
@@ -22,12 +25,36 @@ def solution(n, computers):
         if(in_check):
             continue
         add = [i]
+        out_check = True
         for c in connect:
             if(i in c):
                 if(c[0] == i):
-                    add.append(c[1])
+                    if(check[c[1]] ==-1):
+                        add.append(c[1])
+                        check[c[1]]=(count)
+                        check[c[0]]=(count)
+                        count += 1
+                    else:
+                        out_check = False
+                        temp = answer[check[c[1]]]
+                        temp.append(c[0])
+                        answer[check[c[1]]] = temp
+                        check[c[0]] = check[c[1]]
                 else:
-                    add.append(c[0])
+                    if(check[c[0]] ==-1):
+                        add.append(c[0])
+                        check[c[0]] = count
+                        check[c[1]] = count
+                        count +=1
+                    else:
+                        out_check = False
+                        temp = answer[check[c[0]]]
+                        temp.append(c[1])
+                        answer[check[c[0]]] = temp
+                        check[c[1]] = check[c[0]]
+        if(out_check):
+            answer.append(add)
+    print("answer", answer)
     return len(answer)
 
 print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
