508558206bc60287640786e5decdc554180dd618
chea-young <study.lchy@gmail.com>
2020-08-15 20:15:20 +0900

    MODIFY 예상 대진표.py  이중 반복문 삭제

--- programmers/python/level2/예상 대진표.py
+++ programmers/python/level2/예상 대진표.py
@@ -15,30 +15,34 @@ def solution(n,a,b):
             arr.append(b)
         else:
             arr.append(one)
-            
-    answer =1
+    temp = []
+    number = 0
+    answer = 1
     while(True):
-        temp = []
-        number = 0
-        while(number<len(arr)):
-            one = arr[number]
-            if(number+1 == len(arr)):
-                temp.append(one)
-                break
+        one = arr[number]
+        two = -1
+        if(number+1 != len(arr)):
             two = arr[number+1]
             number +=2
-            if(one== a and two == b):
-                return answer +1
-            elif(two == a and one == a):
-                return answer +1
-            elif(one== a or two == a):
-                temp.append(a)
-            elif(one== b or two == b):
-                temp.append(b)
-            else:
-                temp.append(one)
-        arr=temp
-        answer +=1
+        else:
+            number +=1
+        if(one== a and two == b):
+            return answer +1
+        elif(two ==-1):
+            temp.append(one)
+        elif(two == a and one == a):
+            return answer +1
+        elif(one== a or two == a):
+            temp.append(a)
+        elif(one== b or two == b):
+            temp.append(b)
+        else:
+            temp.append(one)
+        if(number == len(arr)):
+            arr=temp
+            answer+=1
+            temp = []
+            number = 0
 
 print(solution(7,4,7))
 print(solution(10,4,7))
