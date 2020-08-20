3324dd4dcaebe8697f0fc0a548d18e347f3bd70c
chea-young <study.lchy@gmail.com>
2020-08-20 16:15:43 +0900

    MODIFY 큰수 만들기.py 같은 수 연속이면 같이 처리하기 추가

--- programmers/python/level2/큰수 만들기.py
+++ programmers/python/level2/큰수 만들기.py
@@ -1,11 +1,27 @@
 def solution(number, k):
+    answer_num = len(number)-k
     answer = []
     num = [i for i in number]
     max_num = num.index(max(num[:k]))
-    answer.append(num[max_num])
-    del num[:max_num+1]
     k -= max_num
-    # 통과
+    check = num[max_num]
+    answer.append(check)
+    del num[:max_num+1]
+    plus = k-1
+    c = True
+    for i in range (k):
+        if(check != num[i]):
+            c = False
+            plus = i
+            break
+    if(plus !=0):
+        answer += num[:plus]
+        del num[:plus]
+        if(len(answer) >= answer_num):
+            ans = ''
+            for i in answer[:answer_num]:
+                ans += i
+            return ans
     while(k!=0):
         try:
             max_num = num.index(max(num[:k+1]))
@@ -27,3 +43,4 @@ def solution(number, k):
 print(solution("1924",2))
 print(solution("1231234",3))
 print(solution("4177252841",4))
+print(solution("999999999999999999", 10))
