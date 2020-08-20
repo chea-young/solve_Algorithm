8044ebb95bbc0e7dd7d4d1c9837b046a6f45bc2c
chea-young <study.lchy@gmail.com>
2020-08-20 19:37:33 +0900

    MODIFY 큰수만들기.py 10 시간초과

--- programmers/python/level2/큰수 만들기.py
+++ programmers/python/level2/큰수 만들기.py
@@ -2,35 +2,13 @@ def solution(number, k):
     answer_num = len(number)-k
     answer = []
     num = [i for i in number]
-    max_num = num.index(max(num[:k]))
-    k -= max_num
-    check = num[max_num]
-    answer.append(check)
-    del num[:max_num+1]
-    plus = k-1
-    c = True
-    for i in range (k):
-        if(check != num[i]):
-            c = False
-            plus = i
-            break
-    if(plus !=0):
-        answer += num[:plus]
-        del num[:plus]
-        if(len(answer) >= answer_num):
-            ans = ''
-            for i in answer[:answer_num]:
-                ans += i
-            return ans
+    max_num = 0
     while(k!=0):
-        try:
-            max_num = num.index(max(num[:k+1]))
-        except ValueError:
-            num = answer + num
-            max_num = num.index(max(num[:k+1]))
-        if(k<max_num):
-            index = max_num
-            continue
+        max_n = max(num[:k+1])
+        if(max_n == min(num[:k+1])):
+            num = num[k:]
+            break
+        max_num = num.index(max_n)
         answer.append(num[max_num])
         del num[:max_num+1]
         k -= max_num
@@ -43,4 +21,4 @@ def solution(number, k):
 print(solution("1924",2))
 print(solution("1231234",3))
 print(solution("4177252841",4))
-print(solution("999999999999999999", 10))
+print(solution("111211111",4))
