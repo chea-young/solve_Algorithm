7b7ba2d3ac77cce5910f7164c07017e6a8ad2aa9
chea-young <study.lchy@gmail.com>
2020-08-21 20:12:38 +0900

    MODIFY 조이스틱.py 다시 돌아가는거 테스트 케이스11 수정중

--- programmers/python/level2/조이스틱.py
+++ programmers/python/level2/조이스틱.py
@@ -3,55 +3,68 @@ def solution(name):
     b = 'AZYXWVUTSRQPON'
     word = [i for i in name]
     count = []
-    num = 0
-    if(word[0] in f):
-        num = f.index(word[0])+1
+    def count_num(start, end, n):
+        move = 0
+        num = 0
+        check_a = 0
+        check = False
+        if(word[0] in f):
+            num = f.index(word[0])+1
+        else:
+            num = b.index(word[0])+1
+        for i in range(start, end, n):
+            move +=1
+            if(word[i] in f):
+                num += f.index(word[i])
+            else:
+                num += b.index(word[i])
+            if(word[i] == 'A'):
+                check_a += 1
+                move -=1
+                check = True
+            if(check and word[i] != 'A'):
+                check = False
+                move += check_a
+                check_a = 0
+        return (move, num)
+    num_a = []
+    a = 0
+    check = False
+    start = 0
+    for i in range(len(word)):
+        if(word[i] == 'A'):
+            a+=1
+            check = True
+            start = i
+        else:
+            check = False
+            if(a>=2):
+                num_a.append((a,start,i))
+            a=0
+    num_a = sorted(num_a,key=lambda x: x[0], reverse = True)
+    check = False
+    if(len(word[:num_a[0][1]])<num_a[0][0] and len(word[num_a[0][2]+1:])<num_a[0][0]):
+        check = True
+    if(len(word[:num_a[0][1]])<len(word[num_a[0][2]+1:])):
+        num = len(word[:num_a[0][1]])+count_num(num_a[0][1]-1, -1,-1)+1+count_num(len(word)-1,num_a[0][2],-1)
     else:
-        num = b.index(word[0])+1
-    start = num
-    check_a = 0
-    check = False
-    move_r = 0
-    for i in range(1, len(word)):
-        move_r +=1
-        if(word[i] in f):
-            num += f.index(word[i])
-        else:
-            num += b.index(word[i])
-        if(word[i] == 'A'):
-            check_a += 1
-            move_r -=1
-            check = True
-        if(check and word[i] != 'A'):
-            check = False
-            move_r += check_a
-            check_a = 0
+        pass
+
+
+
+    move_r, num = count_num(1,len(word),1)
     count.append(num+move_r)
-    check_a = 0
-    check = False
-    num = start
-    move_l = 0
-    for i in range(len(word)-1, 0, -1):
-        move_l +=1
-        if(word[i] in f):
-            num += f.index(word[i])
-        else:
-            num += b.index(word[i])
-        if(word[i] == 'A'):
-            check_a += 1
-            move_l -=1
-            check = True
-        if(check and word[i] != 'A'):
-            check = False
-            move_l += check_a
-            check_a = 0
+    move_l, num = count_num(len(word)-1,0,-1)
     count.append(num+move_l)
     if(move_r == move_l):
         return count[0] -1
     return min(count)-1
+
+
 
 print(solution("JEROEN")) #56
 print(solution("JAN")) #23
 print(solution("JANN")) #37
 print(solution("JNAN")) # 38
 print(solution("BBBAAAB"))#10
+print(solution("ABABAAAAABA")) #11
