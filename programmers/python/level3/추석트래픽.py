d5cdee390f2545914e09d0ad97f91d2ad7c47829
chea-young <study.lchy@gmail.com>
2020-08-30 12:11:37 +0900

    MODIFY  추석트래픽.py  시간초과

--- programmers/python/level3/추석트래픽.py
+++ programmers/python/level3/추석트래픽.py
@@ -5,13 +5,41 @@ def solution(lines):
         info = lines[i].split()
         time_array = info[1].split(':')
         time = (float(time_array[0])*60+float(time_array[1]))*60+float(time_array[2])
-        pre_time = time-float(info[2][:-1])
-        times.append((round(time-float(info[2][:-1])+0.001,3),time))
-    # 배열 만들어서 넣기
-
+        times.append((int(round(time-float(info[2][:-1]),3)*1000)+1,int(time*1000)+1000))
+    times.sort()
     # 최대값 구하기
-
+    time_in = []
+    now = times[0][0]
+    answer = 0
+    count = 0
+    while(True):
+        if(times == [] and time_in == []):
+            break
+        for t in times:
+            if(now == t[0]):
+                time_in.append(t[1])
+                count+=1
+                times.remove(t)
+        for t in time_in:
+            if(now==t):
+                count -=1
+                time_in.remove(t)
+        if(count>answer):
+            answer = count
+        now += 1
+    return answer
+        
 print(solution(["2016-09-15 01:00:04.001 2.0s",
 "2016-09-15 01:00:07.000 2s"]))
 print(solution(["2016-09-15 01:00:04.002 2.0s",
 "2016-09-15 01:00:07.000 2s"]))
+print(solution(["2016-09-15 20:59:57.421 0.351s",
+"2016-09-15 20:59:58.233 1.181s",
+"2016-09-15 20:59:58.299 0.8s",
+"2016-09-15 20:59:58.688 1.041s",
+"2016-09-15 20:59:59.591 1.412s",
+"2016-09-15 21:00:00.464 1.466s",
+"2016-09-15 21:00:00.741 1.581s",
+"2016-09-15 21:00:00.748 2.31s",
+"2016-09-15 21:00:00.966 0.381s",
+"2016-09-15 21:00:02.066 2.62s"]))
