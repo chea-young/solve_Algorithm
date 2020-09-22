5c0539f50abee9369dee1f57b4378e672860434f
chea-young <study.lchy@gmail.com>
2020-09-22 12:55:19 +0900

    ADD 멀쩡한 사각형.py

---
+++ programmers/python/level2/멀쩡한 사각형.py
@@ -0,0 +1,9 @@
+def solution(w,h):
+    gcd = 0
+    for i in range(min(w,h),0,-1):
+        if(w%i == 0 and h%i ==0):
+            gcd = i
+            break
+    return w*h-gcd*(w//gcd+h//gcd-1)
+
+print(solution(8,12))
