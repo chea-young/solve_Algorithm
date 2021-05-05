9e96df4701dfc353e3ab7369550c4e0a98695aee
cheayoung <lchy0413@gmail.com>
2021-05-05 09:48:00 +0000

    feat: add 음양 더하기.py

---
+++ programmers/python/level1/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer += (-1)*absolutes[i]
+    return answer
