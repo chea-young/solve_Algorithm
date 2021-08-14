3e276468442729f1a5725e4a154c806101fcf70a
chea-young <63410509+chea-young@users.noreply.github.com>
2021-08-14 11:34:17 +0900

    fix: modify 2Q

--- codetree.ai/최고의 13위치 2.py
+++ codetree.ai/최고의 13위치 2.py
@@ -4,7 +4,7 @@ grid = [[0 for i in range(num)] for j in range(num)]
 for col in range(num):
     n = list(map(int, input().split()))
     grid[col] = n
-    
+
 def check_num (col, row):
     global grid
     count = 0
@@ -15,35 +15,14 @@ def check_num (col, row):
             count +=1
     return count
 
-def check_overlap(ele1, ele2):
-    if ele1[1] != ele2[1]:
-        return ele1[0] + ele2[0]
-    if ele1[2]+3 <= ele2[2]:
-        return ele1[0] + ele2[0]
-    else:
-        return None
 
 check = []
-def find_maxnum(first, second, maxnum):
-    global check
-    if first == second and first+1 == len(check):
-        return maxnum
-    elif second+1 == len(check):
-        return find_maxnum(first+1, 0, maxnum)
-    elif first == second:
-        return find_maxnum(first, second+1, maxnum)    
-    else:
-        temp = check_overlap(check[first], check[second])
-        if temp != None:
-            maxnum = max(maxnum, temp)
-        return find_maxnum(first, second+1, maxnum)
-
 for col in range(num):
     for row in range(num):
         temp = check_num(col, row)
         if temp == None:
             break
-        check.append((temp, col, row))
-
-maxnum = find_maxnum(0, 1, 0)
-print(maxnum)
+        check.append(temp)
+        
+check = sorted(check)
+print(check[-1] + check[-2])
