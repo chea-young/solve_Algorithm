466cac3d61fb21ff19c045cbdfee194e0acf8554
chea-young <63410509+chea-young@users.noreply.github.com>
2022-01-01 13:25:09 +0900

    feat: ad 무지의먹방라이브.py

---
+++ programmers/python/level4/무지의먹방라이브.py
@@ -0,0 +1,27 @@
+"""
+N
+1번 -> 번호 증가
+마지막 -> 1번
+1초 -> 다음 음식 (가장 가까운 번호 음식)
+
+"""
+import heapq
+
+def solution(food_times, k):
+    
+    # data 정리
+    data = [] #round, order, time
+    for f in range(len(food_times)):
+        heapq.heappush(data, [0, f, food_times[f]])
+    
+    # k초 전까지 음식 섭취
+    for i in range(0, k):
+        if not data: # 섭취할 음식이 없는 경우
+            return -1 
+        now_round, now_order, now_time = heapq.heappop(data)
+        if now_time - 1 > 0:
+            heapq.heappush(data, [now_round+1, now_order, now_time-1])
+            
+    # k초 후에 섭취할 음식 찾기
+    answer = heapq.heappop(data)
+    return answer[1]+1
