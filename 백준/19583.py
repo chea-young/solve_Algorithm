d3f812f0a25c4853bc7226310ef643029142927e
chea-young <63410509+chea-young@users.noreply.github.com>
2022-02-18 20:58:46 +0900

    fix: modify 19583.py

--- 백준/19583.py
+++ 백준/19583.py
@@ -1,4 +1,6 @@
 # 싸이버개강총회
+import sys
+input = sys.stdin.readline
 # 입력 0000 ex 23:14 -> 2314
 start, end_event, end_stream = [int(i[:2] + i[3:]) for i in input().split()]
 input_time = 0
@@ -10,13 +12,10 @@ while True:
         input_time = int(ele[0][:2] + ele[0][3:])
         name = ele[1]
         
-        if input_time <= start: # 개강 총회 전에 들어온 인원
+        if input_time <= start: # 개강총회를 시작하기 전에 들어온 인원
             in_list.add(name)
-        elif input_time < end_event: # 개강 총회 끝나기 전에 나간 사람
-            in_list.remove(name)
-        else: # 개강 총회 끝나고 나가는 사람
+        elif end_event <= input_time <= end_stream: # 개강총회를 끝내고 나서, 스트리밍을 끝낼 때까지 인원
             out_list.add(name)
-            
     except:
+        print(len(in_list & out_list)) # 출력
         break      
-print(len(in_list & out_list)) # 출력
