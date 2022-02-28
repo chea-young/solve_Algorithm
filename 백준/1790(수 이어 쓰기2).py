035004d9e61e92e08b4e0d5f94df8e369153894f
chea-young <63410509+chea-young@users.noreply.github.com>
2022-02-28 18:13:36 +0900

    fix: modify 메모리초과

--- 백준/1790(수 이어 쓰기2).py
+++ 백준/1790(수 이어 쓰기2).py
@@ -2,14 +2,20 @@
 
 # 입력
 N, k = map(int, input().split())
+digit = 1
+nine = 9
+answer = 0
 
-# 이어진 숫자 list 만들기
-data = ''
-for i in range(1, N+1):
-    data += str(i)
-    
-#출력
-try:
-    print(data[k-1])
-except IndexError:
+#k 위치 구하기
+while k > digit*nine:
+    k = k - (digit*nine) # 이전 부분 제거
+    answer = answer + nine
+    # 자릿 수 추가
+    digit += 1
+    nine = nine * 10
+
+answer = (answer+1) + ((k-1)//digit)
+if answer > N:
     print(-1)
+else:
+    print(str(answer)[(k-1)%digit])
