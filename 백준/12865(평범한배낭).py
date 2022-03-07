59a4904f43f67b5b6997c700c6556a04288be48e
chea-young <63410509+chea-young@users.noreply.github.com>
2022-03-07 18:56:27 +0900

    fix: modify the error

--- 백준/12865(평범한배낭).py
+++ 백준/12865(평범한배낭).py
@@ -5,10 +5,17 @@ N, K = map(int, input().split())
 data = [ [int(j) for j in input().split()] for i in range(N)]
 
 dp = [0]*(K+1) # 0부터 K 무게까지 저장
-for i in range(K+1):
-    for ele in data:
-        if i-ele[0] >= 0:
-            dp[i] = max(dp[i], dp[i-ele[0]]+ele[1])
-
+for ele in data: -> 정답
+    print(dp)
+    for i in range(K, ele[0]-1,-1):
+        dp[i] = max(dp[i], dp[i-ele[0]]+ele[1])   
+        
+print('--------------')
+dp = [0]*(K+1) # 0부터 K 무게까지 저장
+for ele in data: -> 오답
+    print(dp)
+    for i in range(ele[0], K+1):
+        dp[i] = max(dp[i], dp[i-ele[0]]+ele[1])          
+        
 # 출력
 print(dp[-1])
