c4a38633676675d1a14b79fa0b66bd79cb2be1aa
chea-young <63410509+chea-young@users.noreply.github.com>
2022-03-09 13:42:47 +0900

    fix: modify MemoryError

--- 백준/2110(공유기설치).py
+++ 백준/2110(공유기설치).py
@@ -3,29 +3,29 @@
 # 입력
 N, C = map(int, input().split())
 data = [int(input()) for i in range(N)]
-f, end = min(data), max(data)
+f, end = data.index(min(data)), data.index(max(data))
 
-installed = [0] * (end+1)
+installed = [0] * N
 installed[f] = 1
 installed[end] = 1
 in_list = [f, end]
 
 def find_dis(num): # 가장 인접한 공유기 사이 구하는 함수
     dis_list = []
-    for ele in in_list:
-        dis_list.append(abs(ele-num))
+    for i in in_list:
+        dis_list.append(abs(data[i]-num))
     return min(dis_list)
 
 for count in range(C-2):
     dis = 0
     check_i = 0
     for i, ele in enumerate(data):
-        if installed[ele] == 1:
+        if installed[i] == 1:
             continue
         dis_i = find_dis(ele)
         if dis_i > dis:
             dis = dis_i
-            check_i = ele
+            check_i = i
     installed[check_i] = 1
     in_list.append(check_i)
     
@@ -34,5 +34,5 @@ answer = end-f
 for i in in_list:
     for j in in_list:
         if i != j:
-            answer = min(answer, abs(i-j))
+            answer = min(answer, abs(data[i]-data[j]))
 print(answer)
