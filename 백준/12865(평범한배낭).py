2ce2a21be0656ea135939d2ed087add36937a113
chea-young <63410509+chea-young@users.noreply.github.com>
2022-03-10 23:52:58 +0900

    feat: add solution

--- 백준/12865(평범한배낭).py
+++ 백준/12865(평범한배낭).py
@@ -5,7 +5,7 @@ N, K = map(int, input().split())
 data = [ [int(j) for j in input().split()] for i in range(N)]
 
 dp = [0]*(K+1) # 0부터 K 무게까지 저장
-for ele in data: -> 정답
+for ele in data:
     print(dp)
     for i in range(K, ele[0]-1,-1):
         dp[i] = max(dp[i], dp[i-ele[0]]+ele[1])   
@@ -15,6 +15,7 @@ dp = [0]*(K+1) # 0부터 K 무게까지 저장
 for ele in data: -> 오답
     print(dp)
     for i in range(ele[0], K+1):
+        # if문 추가해서 수정하기
         dp[i] = max(dp[i], dp[i-ele[0]]+ele[1])          
         
 # 출력
