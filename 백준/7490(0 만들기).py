34ca8d2aed3331b10c4ec8c422a5ce1fdc2bcd84
chea-young <63410509+chea-young@users.noreply.github.com>
2022-03-16 15:42:57 +0900

    feat: add 'ASCII 정럴'

--- 백준/7490(0 만들기).py
+++ 백준/7490(0 만들기).py
@@ -2,7 +2,6 @@
 
 # 입력
 T = int(input())
-
 for _ in range(T):
     num = int(input())
     # 모든 경우의 수 구하기
@@ -14,8 +13,14 @@ for _ in range(T):
                data.append(ele+m+str(n))
         all_data = data
     
+    answer = []
     # 식 계산하기
     for ele in all_data:
         cal = ele.replace(' ','') # 계산을 위해 문자열 변경
         if eval(cal) == 0:# eval : 문자열 식의 합을 구하는 모듈
-            print(ele) # 출력
+            answer.append(ele)
+    
+    # ASCII 순서로 정렬
+    answer = sorted(answer)
+    for ele in answer:
+        print(ele) # 출력
