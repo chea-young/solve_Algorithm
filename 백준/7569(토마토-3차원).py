ecf1138e8414c67ca4f216e16b89e1aa48aba60d
chea-young <63410509+chea-young@users.noreply.github.com>
2022-03-16 15:54:12 +0900

    fix: change input method

--- 백준/7569(토마토-3차원).py
+++ 백준/7569(토마토-3차원).py
@@ -4,23 +4,21 @@
 # 위 아래 왼쪽 오른쪽 앞 뒤
 # 1 -> 익은 0 -> 익지 X, -1 -> 없는 거
 # 최소 일 수!!
-
+import sys
+from collections import deque
 # 입력
 M, N, H = map(int, input().split())
 boxes = []
-check = [] # 하루에 익어야하는 토마토들
+check = deque([]) # 하루에 익어야하는 토마토들
 visited = [[[0 for _ in range(M)] for __ in range(N)] for _ in range(H)]
 for h in range(H):
     data = []
     for n in range(N):
-        ele = []
-        word = input().split()
+        data.append(list(map(int,sys.stdin.readline().split())))
         for m in range(M):
-            ele.append(int(word[m]))
-            if word[m] == '1' :
+            if data[n][m] == 1 :
                 check.append([h, n, m])
                 visited[h][n][m] = 1
-        data.append(ele)
     boxes.append(data)       
             
 # 위 아래 왼쪽 오른쪽 앞 뒤
@@ -28,9 +26,9 @@ d = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
 answer = -1 # 날짜! -> 첫날부터 +1이 되기 때문에 
 while check:
     answer += 1
-    next_check = []
+    next_check = deque([])
     while check:
-        now = check.pop(0)
+        now = check.popleft()
         for move in d: # 위 아래 왼쪽 오른쪽 앞 뒤 -> 확인
             z = now[0] + move[0]
             y = now[1] + move[1]
@@ -41,7 +39,7 @@ while check:
                     boxes[z][y][x] = 1
                     next_check.append([z, y, x])
     check = next_check
-    
+
 # 다 익었는지 확인
 count = 0
 for data in visited:
