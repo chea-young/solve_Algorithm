304f34b71af27fdd739e3d6875b8567f5739cf5f
chea-young <63410509+chea-young@users.noreply.github.com>
2022-03-18 15:13:19 +0900

    feat: add N>2 case

--- 백준/17404(RGB거리2).py
+++ 백준/17404(RGB거리2).py
@@ -5,16 +5,16 @@
 
 # 입력
 N = int(input())
-data = [[int(j) for j in input().split()]for i in range(N)]
+data = [[int(j) for j in input().split()] for i in range(N)]
 
-answer = 1001
+answer = 10001
 for initcolor in range(3): # 첫 번째 색이 정해진 상태
     dp = [[0 for j in range(3)]for i in range(N)]    
 
     # 2번째 채우기
     for i in range(3):
         if initcolor == i:
-            dp[1][i] = 1001
+            dp[1][i] = 10001
             continue# 선택이 안되게 하기 위해서
         dp[1][i] = data[0][initcolor] + data[1][i]
         
@@ -28,14 +28,14 @@ for initcolor in range(3): # 첫 번째 색이 정해진 상태
         dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + data[i][1]
         # blue
         dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + data[i][2]
-    
-    # 마지막 채우기
-    for i in range(3):
-        if initcolor == i: # 첫번째와 이전이 같을 때
-            dp[N-1][i] = 1001 # 선택이 안되게 하기 위해서
-            continue
-        dp[N-1][i] = min(dp[N-2][(i+1)%3], dp[N-2][(i+2)%3]) + data[N-1][i]
+        
+    if N > 2:
+        # 마지막 채우기
+        for i in range(3):
+            if initcolor == i: # 첫번째와 이전이 같을 때
+                dp[N-1][i] = 10001 # 선택이 안되게 하기 위해서
+                continue
+            dp[N-1][i] = min(dp[N-2][(i+1)%3], dp[N-2][(i+2)%3]) + data[N-1][i]
     answer = min(answer, min(dp[N-1]))
-
 # 출력
 print(answer)
