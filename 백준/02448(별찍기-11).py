da8a69bfa36ca8c593219d318af1bcf592ae1287
chea-young <63410509+chea-young@users.noreply.github.com>
2022-03-23 17:39:16 +0900

    feat: add 02448(별찍기-11).py

---
+++ 백준/02448(별찍기-11).py
@@ -0,0 +1,28 @@
+# 2448 별 찍기 - 11
+
+# 입력
+N = int(input())
+data = [[' ' for i in range(2*N)] for i in range(N)]
+
+def print_star(y, x, n):
+#   . -> y,x
+#   *
+#  * *
+# *****
+    if n ==3:
+        data[y][x] = '*'
+        data[y+1][x+1] = '*'
+        data[y+1][x-3] = '*'
+        for i in range(-2, 3):
+            data[y+2][x+i] = '*'
+    else:
+        plus = n//2
+        print_star(y, x, plus)
+        print_star(y+plus, x+plus, plus)
+        print_star(y+plus, x-plus, plus)
+            
+print_star(0, N-1, N)
+
+# 출력
+for ele in data:
+    print("".join(ele))
