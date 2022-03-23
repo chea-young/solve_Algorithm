5d4ce0f5a45b4cea9b4cf0342e6c04229cb1584f
chea-young <63410509+chea-young@users.noreply.github.com>
2022-03-23 17:43:53 +0900

    fix: change output-type

--- 백준/02448(별찍기-11).py
+++ 백준/02448(별찍기-11).py
@@ -1,9 +1,5 @@
 # 2448 별 찍기 - 11
 
-# 입력
-N = int(input())
-data = [[' ' for i in range(2*N)] for i in range(N)]
-
 def print_star(y, x, n):
 #   . -> y,x
 #   *
@@ -12,7 +8,7 @@ def print_star(y, x, n):
     if n ==3:
         data[y][x] = '*'
         data[y+1][x+1] = '*'
-        data[y+1][x-3] = '*'
+        data[y+1][x-1] = '*'
         for i in range(-2, 3):
             data[y+2][x+i] = '*'
     else:
@@ -20,6 +16,10 @@ def print_star(y, x, n):
         print_star(y, x, plus)
         print_star(y+plus, x+plus, plus)
         print_star(y+plus, x-plus, plus)
+
+# 입력
+N = int(input())
+data = [[' ' for i in range(2*N)] for i in range(N)]
             
 print_star(0, N-1, N)
 
