2c1a29bd5dc551f8b49b0004c05d0ef25dd9ac7d
chea-young <63410509+chea-young@users.noreply.github.com>
2022-05-05 20:24:43 +0900

    fix: modify 알고리즘

--- 백준/01158(요세푸스문제).py
+++ 백준/01158(요세푸스문제).py
@@ -8,16 +8,11 @@ N, K = map(int, input().split())
 answer = []
 data = [i+1 for i in range(N)]
 now = 0
-check_num = 1
 while data:
-    plus_in = now + K -1
-    del_in = plus_in%len(data)
-    check = plus_in//len(data)
-    answer.append(data[del_in])
-    del data[del_in]
-    
-    if check == 0:
-        now= (now+1)%len(data)
-        
+    now += K-1
+    now = now%len(data)
+    answer.append(data[now])
+    del data[now]
+
 # 출력
 print(f'<{", ".join(map(str, answer))}>')
