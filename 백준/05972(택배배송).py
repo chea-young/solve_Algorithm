394947ae69b3a6a68eb8a61178d35b680b61937e
chea-young <63410509+chea-young@users.noreply.github.com>
2022-05-05 18:17:03 +0900

    fix: change deque to heapq

--- 백준/05972(택배배송).py
+++ 백준/05972(택배배송).py
@@ -1,6 +1,6 @@
 #05972 택배 배송
 
-from collections import deque
+import heapq
 # 입력
 N, M = map(int, input().split())
 
@@ -12,20 +12,20 @@ for ele in range(M):
     conn[node1].append([node2, weight])
     conn[node2].append([node1, weight])
 
-queue = deque()
-queue.append((1, 0)) # 헛간, 소
+queue = []
+heapq.heappush(queue, (1, 0))
 visited[1] = 0
 while queue:
-    check = queue.popleft()
+    check = heapq.heappop(queue)
     for ele in conn[check[0]]:
         new_weight = check[1] + ele[1]
         if not visited[ele[0]]:
             visited[ele[0]] = new_weight
-            queue.append([ele[0], new_weight])
+            heapq.heappush(queue, (ele[0], new_weight))
         else:
             if visited[ele[0]] > new_weight: 
                 visited[ele[0]] = new_weight
-                queue.append([ele[0], new_weight])
+                heapq.heappush(queue, (ele[0], new_weight))
 
 #출력
 print(visited[-1])
