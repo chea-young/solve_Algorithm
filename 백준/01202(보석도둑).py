fd0d2843adbbc5df078ca2d9ef0f537b65788e30
chea-young <63410509+chea-young@users.noreply.github.com>
2022-06-06 14:26:15 +0900

    fix: modify TimeErorr

--- 백준/01202(보석도둑).py
+++ 백준/01202(보석도둑).py
@@ -1,29 +1,25 @@
-
+import heapq
+import sys
+ 
 #입력
-N, K = map(int, input().split())
-jewels = [list(map(int, input().split())) for _ in range(N)]
-bags = [int(input()) for _ in range(K)]
-
+N, K = map(int, sys.stdin.readline().split())
+jewels = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+bags = [int(sys.stdin.readline()) for _ in range(K)]
+ 
 # 정렬
 jewels.sort()
 bags.sort()
-
+ 
 # 가방의 무게보다 작거나 같은 보석에서 최대 가치인 보석 찾기
 answer = 0
+data = []
 for b in bags:
-    max_data = [] # (index, value)
-    for i, j in enumerate(jewels):
-        m, v = j
-        if m <= b:
-            if not max_data or max_data[1] < v:
-                max_data = [i, v]
+    while jewels and b >= jewels[0][0]:
+        heapq.heappush(data, (-1)*jewels[0][1])
+        heapq.heappop(jewels)
                 
-    # 가방 무게보다 작은 보석이 없는 경우
-    if not max_data:
-        continue
-    answer += max_data[1]
-    del jewels[max_data[0]]
-    
-
+    if data: # 가방에 넣을 수 있는 보석이 있는 경우
+        answer += (-1) * heapq.heappop(data)
+        
 # 출력
 print(answer)
