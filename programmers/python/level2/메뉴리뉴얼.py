4f9950f97ced43f683340967b7716727f9889df5
chea-young <63410509+chea-young@users.noreply.github.com>
2022-06-17 15:19:12 +0900

    feat: add 메뉴리뉴얼.py

--- programmers/python/level2/메뉴리뉴얼.py
+++ programmers/python/level2/메뉴리뉴얼.py
@@ -1,44 +1,57 @@
-from itertools import combinations
-
+def findCase(c, order): 
+    case = []
+    order = sorted(order)
+    used = [0 for _ in range(len(order))]
+ 
+    def generate(chosen):
+        if len(chosen) == c:
+            case.append(tuple(chosen.copy()))
+ 
+        start = order.index(chosen[-1]) + 1 if chosen else 0
+        for nxt in range(start, len(order)):
+            if used[nxt] == 0 and (nxt == 0 or order[nxt-1] != order[nxt] or used[nxt-1]):
+                chosen.append(order[nxt])
+                used[nxt] = 1
+                generate(chosen)
+                chosen.pop()
+                used[nxt] = 0
+    generate([])
+    return case
+ 
+ 
 def solution(orders, course):
     answer = []
-    menu = []
-    order_people = []
-
-    for num in range(len(orders)):
-        for alpha in orders[num]:
-            if(alpha not in menu):
-                menu.append(alpha)
-                order_people.append([num])
-            else:
-                i = menu.indexof(alpha)
-                l = order_people[i]
-                l.append(num)
-                order_people[i] = l
-    
-    for num in course:
-        type = list(map(''.join, combinations(menu, num)))
-        check = []
-        for type_item in type:
-            # 각 리스트 찾기
-            alpha_list = []
-            for c in type_item:
-                i = menu.indexof(c)
-                check.append(order_people[i])
-            # 교집합하기
-            first = alpha_list[0]
-            for c in range(1,len(alpha_list)):
-                first = first.intersection(alpha_list[c])
-                if(len(first)==1):
-                    break
-            if(len(first)!=1):
-                check.append((len(first), type_item))
-        check = check.reverse()
-        max_num  = check[0][0]
-        answer.append(check[0][1])
-        for i in range (1, len(check)):
-            if(check[i][0] != max_num):
+    # 가능한 메뉴 구성 구하
+    orderCase = {c: [] for c in course} # {메뉴구성개수: [구성조합]}
+    for order in orders:
+        for c in course:
+            # 원하는 조합의 개수보다 주문 수가 작은 경우 
+            if len(order) < c:
                 break
-            answer.append(check[i][1])
-        return answer
-    return answer
+            
+            # 구성 조합 구하기
+            orderCase[c] += findCase(c, order)
+    
+    # 메뉴 불린 횟수 저장하기
+    orderMenu = {} # {메뉴구성개수: {구성조합: 주문 수}}
+    for c in course:
+        caseCnt = {}
+        for ele in orderCase[c]:
+            caseCnt[ele] = caseCnt.get(ele, 0) + 1
+        orderMenu[c] = caseCnt
+    
+    # 제일 많이 불린 음식 구하기
+    orderMenuSorted = {c: sorted([(v, k) for k, v in orderMenu[c].items() if v > 1]) for c in course} # {메뉴구성개수: [주문수, 구성조합]}
+    for c in course:
+        cData = orderMenuSorted[c]
+        bestCourse = []
+        if cData:
+            bestCourse =  cData.pop()
+            answer.append(''.join(bestCourse[1]))
+        while cData:
+            if bestCourse[0] ==  cData[-1][0]:
+                answer.append(''.join(cData.pop()[1]))
+            else:
+                break
+            
+    return sorted(answer)
