4bcec60569f5a8fb7b84e66410b7d34503f4d687
chea-young <63410509+chea-young@users.noreply.github.com>
2022-07-26 00:38:29 +0900

    feat: add 09935(문자열폭발).py

---
+++ 백준/09935(문자열폭발).py
@@ -0,0 +1,38 @@
+#9935 문자열폭발
+
+"""
+폭발 문자 -> 폭발 -> 합쳐짐
+과정
+    존재 0 -> 
+        모든 폭발 문자 폭발 -> 남은 문자열 합체
+    존재 X -> 반복 
+
+"""
+
+# 폭발 문자열을 폭발 시키는 함수
+def check_bomb(word, bomb):
+    now = 0 # 현재 비교하는 첫 번째 index
+    len_bomb = len(bomb)
+    while now < len(word) - len_bomb + 1:
+        if word[now:now+len_bomb] == bomb:
+            word  = word[:now] + word[now+len_bomb:]
+        else:
+            now += 1
+    return word
+# 입력
+word = input()
+bomb = input()
+
+while True:
+    # 폭발 문자열 확인
+    now_word = check_bomb(word, bomb)
+
+    # 출력
+    if not word: # 남아있는 문자가 없는 경우
+        print("FRULA")
+        break
+    elif now_word == word: # 폭발 문자열이 없는 경우
+        print(now_word)
+        break
+
+    word = now_word
